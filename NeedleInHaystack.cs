2ee68397c6d66380f1fe2727a511b1e1dee056de
BenPease21 <BAJA154@outlook.com>
2025-04-01 13:19:27 -0500

    Initial commit

---
+++ NeedleInHaystack.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        for (int i=0; i<(haystack.Length - needle.Length); i++)
+        {
+            if (haystack.Substring(i, needle.Length) == needle)
+            {
+                return i;
+            }
+
+        }
+
+        return -1;
+    }
+
+    static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.StrStr("hello", "ll"));
+        
+    }
+}
